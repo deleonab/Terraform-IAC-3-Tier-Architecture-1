b6678f814628613ff77320b750db72f990b8ec33
Dele Onabowu <delesodade@hotmail.com>
2022-09-20 13:31:58 +0100

    terraform code updated for vpc and subnets

---
+++ routes.tf
@@ -0,0 +1,25 @@
+resource "aws_route_table" "rtb-public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.d
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
+
+resource "aws_route_table" "rtb-private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "example"
+  }
+}
