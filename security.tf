cba6d4854494f515921836f25b75f6a75cce0151
Dele Onabowu <delesodade@hotmail.com>
2022-09-20 23:11:12 +0100

    updated code for security groups

--- security.tf
+++ security.tf
@@ -32,3 +32,62 @@ resource "aws_security_group" "ext-alb-sg" {
     Name = "ext-alb-sg"
   }
 }
+
+
+
+resource "aws_security_group" "bastion-sg" {
+  name        = "bastion-sg"
+  description = "Allow TLS inbound traffic frm ALB"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    # CIDR block should be restricted to host IP address in production
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "bastion-sg"
+  }
+}
+
+
+
+resource "aws_security_group" "nginx-sg" {
+  name        = "nginx-sg"
+  description = "Allow TLS inbound traffic from ALB"
+  vpc_id      = aws_vpc.main.id
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "nginx-sg"
+  }
+}
+
+resource "aws_security_group_rule" "nginx-sg-rule" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  source_security_group_id = aws_security_group.ext-alb-sg.id
+  security_group_id = aws_security_group.nginx-sg.id
+}
