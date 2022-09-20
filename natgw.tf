03cdf579b77143b847f5137b588c10bdbef9f772
Dele Onabowu <delesodade@hotmail.com>
2022-09-20 12:57:18 +0100

    terraform code updated for vpc and subnets

--- natgw.tf
+++ natgw.tf
@@ -1,18 +1,17 @@
-resource "aws_eip" "eip" {
+resource "aws_eip" "nat_eip" {
   vpc = true
   depends_on                = [aws_internet_gateway.igw]
 }
 
 
 resource "aws_nat_gateway" "natgw" {
-  allocation_id = aws_eip.eip
+  allocation_id = aws_eip.nat_eip
   subnet_id     = aws_subnet.public.id
 
   tags = {
-    Name = "gw NAT"
+    Name = Format("%s-%s",aws_subnet.public.id,"NATGW")
   }
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
+  # To ensure proper ordering, it is recommended to add an explicit dependency on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
 }
